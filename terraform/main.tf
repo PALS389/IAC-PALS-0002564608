d7c9205ba2b4e35816069cb4a3e8a23b62b85f8c
PALS389 <palspiero123@gmail.com>
2026-07-08 19:05:33 -0500

    ahora voy von ansible y actualice el main de terrafor y ademas eh credo una llave secreta pe causha

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,42 +2,40 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1. Creamos el muro de seguridad (Security Group)
+# Creamos la cerradura en AWS leyendo la llave que acabas de generar
+resource "aws_key_pair" "llave_ssh" {
+  key_name   = "llave_boton_caos"
+  public_key = file("~/.ssh/llave_aws.pub")
+}
+
 resource "aws_security_group" "boton_caos_sg" {
   name        = "boton_caos_sg"
   description = "Permitir trafico para la app y monitoreo"
 
-  # Puerto 22 para que Ansible se conecte después
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # Puerto 8000 para tu aplicación de FastAPI
   ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # Puertos 9090 (Prometheus), 3100 (Loki) y 3000 (Grafana)
   ingress {
     from_port   = 3000
     to_port     = 3100
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 9090
     to_port     = 9090
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -46,11 +44,12 @@ resource "aws_security_group" "boton_caos_sg" {
   }
 }
 
-# 2. Pedimos la máquina virtual gratuita
 resource "aws_instance" "servidor_boton" {
-  ami           = "ami-04b70fa74e45c3917" # Imagen oficial de Ubuntu 24.04 en us-east-1
-  instance_type = "t2.micro"              # Capa 100% gratuita
-
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.micro"
+  
+  # Le decimos a la máquina que use la llave de arriba
+  key_name      = aws_key_pair.llave_ssh.key_name
   vpc_security_group_ids = [aws_security_group.boton_caos_sg.id]
 
   tags = {
